57656aad1d351df3578637edd23b552524c49fb9
Adam Marciniak <adam668@gmail.com>
2021-06-09 13:46:52 -0700

    Revert "a"
    This reverts commit d8d96790b531517b7c22ecad66d6192e3643353d.

--- index.js
+++ index.js
@@ -8,12 +8,6 @@ const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({ port: 8080 });
 
-function noop() {}
-
-const heartbeat = () => {
-  this.isAlive = true;
-}
-
 const games = {'abc' : { gameWs: null, clients: []}};
 
 const getRandomString = () => {
@@ -30,6 +24,8 @@ const sendMessageToGame = (message, gameId) => {
 const sendMessageToClients = (message, gameId) => {
   console.log(`Send to clients Message: ${message}, ID: ${gameId}`)
 
+
+
   games[gameId].clients.forEach(client => {
     sendWsMessage(client, message, 'data');
   })
@@ -43,7 +39,7 @@ const sendWsMessage = (ws, message, type) => {
 }
 
 const handleGameConnection = (ws) => {
-  sendWsMessage(ws,ws.gameId, 'gameCode' );
+  sendWsMessage(ws,`${ws.gameId}`, 'gameCode' );
   ws.on('message', (data) => {
     sendMessageToClients(data, ws.gameId);
   })
@@ -56,9 +52,6 @@ const handleClientConnection = (ws) => {
 }
 
 wss.on('connection', (ws, req) => {
-
-  ws.isAlive = true;
-  ws.on('pong', heartbeat);
 
   const paths = req.url.split('/');
   const type = paths[1];
@@ -121,19 +114,9 @@ wss.on('connection', (ws, req) => {
 
   }
 
-});
+  
 
-const interval = setInterval(function ping() {
-  wss.clients.forEach(function each(ws) {
-    if (ws.isAlive === false) return ws.terminate();
-
-    ws.isAlive = false;
-    ws.ping(noop);
-  });
-}, 30000);
-
-wss.on('close', function close() {
-  clearInterval(interval);
+  
 });
 
 const checkIfGameIdExists = (gameId) => {
